39dcd968d8a3867ae84d3502db23343366a94227
Leilaaaa <129416593+ikinaaaa@users.noreply.github.com>
2023-09-08 20:28:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+const copyToClipboard = () => {
+    const contractText = document.getElementById('contract-text');
+    const textToCopy = contractText.innerText;
+  
+    navigator.clipboard.writeText(textToCopy)
+      .then(() => {
+        alert('Copied to clipboard!');
+      })
+      .catch((error) => {
+        console.error('Failed to copy to clipboard:', error);
+      });
+  };
